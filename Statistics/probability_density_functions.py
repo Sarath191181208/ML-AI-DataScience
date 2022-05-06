d8d1c76342026b98cda208b8168b8ee6a66eb540
Sarath <vssarathc04@gmail.com>
2022-05-07 00:03:15 +0530

    created linear function

--- Statistics/probability_density_functions.py
+++ Statistics/probability_density_functions.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 
-def PDF_COS(x, lam=3):
+def PDF_COS(x: np.array, lam: float = 3) -> np.array:
     """ returns the cos of x*lam think of lam as a squishification constant """
     return np.cos(x*lam)
 
 
-def PDF_SIN(x, lam=2):
+def PDF_SIN(x: np.array, lam: float = 2) -> np.array:
     """ returns the sin of x*lam think of lam as a squishification constant """
     return np.sin(x*lam)
+
+
+def func(x: np.array, lam: float = 1) -> np.array:
+    """ returns x*lam think of lam as a squishification constant """
+    return x*lam
