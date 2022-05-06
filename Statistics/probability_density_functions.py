6757f20f20d9fd178533cf6a366c4a241d5e09aa
Sarath <vssarathc04@gmail.com>
2022-05-07 00:23:35 +0530

    Created some PDF's

--- Statistics/probability_density_functions.py
+++ Statistics/probability_density_functions.py
@@ -11,6 +11,21 @@ def PDF_SIN(x: np.array, lam: float = 2) -> np.array:
     return np.sin(x*lam)
 
 
-def func(x: np.array, lam: float = 1) -> np.array:
+def PDF_LINEAR(x: np.array, lam: float = 1) -> np.array:
     """ returns x*lam think of lam as a squishification constant """
     return x*lam
+
+
+def PDF_1BY_E(x: np.array, lam: float = 5) -> np.array:
+    """ returns 1/x think of lam as a squishification constant """
+    return 1/np.exp(x*lam)
+
+
+def PDF_E(x: np.array, lam: float = 5) -> np.array:
+    """ returns e^(lam*x) think of lam as a squishification constant """
+    return np.exp(x*lam)
+
+
+def PDF_90E(x: np.array, lam: float = 5) -> np.array:
+    """ returns an nparray think of lam as a squishification constant """
+    return 1-(x**3*lam)
