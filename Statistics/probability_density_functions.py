f488f97b71f4a8deaa16c0ac26daa65631bfc237
Sarath <vssarathc04@gmail.com>
2022-05-07 00:00:39 +0530

    Created Sin function

--- Statistics/probability_density_functions.py
+++ Statistics/probability_density_functions.py
@@ -4,3 +4,8 @@ import numpy as np
 def PDF_COS(x, lam=3):
     """ returns the cos of x*lam think of lam as a squishification constant """
     return np.cos(x*lam)
+
+
+def PDF_SIN(x, lam=2):
+    """ returns the sin of x*lam think of lam as a squishification constant """
+    return np.sin(x*lam)
